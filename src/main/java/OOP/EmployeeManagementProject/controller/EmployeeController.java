66964855fd27b95d7feee011db447dbb663b7aac
Doğukan Uykun <uykundogukan@gmail.com>
2022-12-01 11:52:39 +0300

    Include controller & initialize react frontend

---
+++ src/main/java/OOP/EmployeeManagementProject/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package OOP.EmployeeManagementProject.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import OOP.EmployeeManagementProject.model.Employee;
+import OOP.EmployeeManagementProject.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	//get all employees
+	@GetMapping("/getAll")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+	
+}
